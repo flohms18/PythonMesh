5ac667629ae6e297b87ccd2c13a3c1730f244740
flohms18 <trouilletflorent@outlook.com>
2026-01-08 16:53:54 +0100

    configure central service with python

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI,HTTPException
+import httpx
+from registry import list_data_products, get_product_url
+
+app = FastAPI(title="Python_Mesh")
+
+@app.get("/products")
+def products():
+    return {
+        "products" : list_data_products
+    }
+
+@app.get("products/{product_name}")
+def fetch_product(product_name: str):
+    url = get_product_url(product_name)
+    if not url:
+        raise HTTPException(status_code=404,detail="Data Product not found")
+    try: 
+        response = httpx.get(url,timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except httpx.HTTPError as e: 
+        raise HTTPException(status_code=500,detail=str(e))
